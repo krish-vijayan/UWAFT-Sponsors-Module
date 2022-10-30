76cbc653bba561e018c6d31992e013d74f426c65
Ahmed Ahmed <ahmedhmagzoub@gmail.com>
2022-10-29 22:14:45 -0400

    added card element but it still needs updating

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import "./App.css";
 import FoundationalCarousel from "./components/Carousel";
+import BasicCard from "./components/Card";
+import Grid2 from '@mui/material/Unstable_Grid2';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <>
       <div className="background">
-        <FoundationalCarousel />;
+        <Grid2 container spacing={3}>
+          <Grid2 xs={6} md={4}>
+            <BasicCard/>
+          </Grid2>
+          <Grid2 xs={6} md={4}>
+          <BasicCard/>
+          </Grid2>
+          <Grid2 xs={6} md={4}>
+          <BasicCard/>
+          </Grid2>
+          <Grid2 xs={6} md={4}>
+            <BasicCard/>
+          </Grid2>
+          <Grid2 xs={6} md={4}>
+          <BasicCard/>
+          </Grid2>
+          <Grid2 xs={6} md={4}>
+          <BasicCard/>
+          </Grid2>
+          <Grid2 xs={6} md={4}>
+            <BasicCard/>
+          </Grid2>
+          <Grid2 xs={6} md={4}>
+          <BasicCard/>
+          </Grid2>
+          <Grid2 xs={6} md={4}>
+          <BasicCard/>
+          </Grid2>
+        </Grid2>
       </div>
     </>
   );
